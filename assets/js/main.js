b766e6f369fe316d9991875ea7638f2f33aca667
lukwiya23 <luk23bonnie8@gmail.com>
2021-06-20 18:13:42 +0300

    ||Responsive Navbar Complete||

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 /*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu')
+const toggleMenu = document.getElementById('nav-toggle')
+const closeMenu = document.getElementById('nav-close')
 
 // SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+    
+})
 
 // HIDDEN
 
-
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
 /*===== MOUSEMOVE HOME IMG =====*/
 
 
